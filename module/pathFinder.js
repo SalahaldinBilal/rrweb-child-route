209b8159c02f8613a42721526ce7fa99493733db
SalahaldinBilal <salahbilal2@hotmail.com>
2021-05-06 14:53:41 +0300

    fixes

--- module/pathFinder.js
+++ module/pathFinder.js
@@ -5,23 +5,22 @@ export default class pathFiner{
 
     changeTree(domTree){
         this.tree = domTree;
-        this.body = this.tree.childNodes[1].childNodes[2]
     }
 
     findPathForElement(id){
-        let finished = false
-        let path = [{id: this.body.id, attributes: this.body.attributes, tagName:this.body.tagName}]
-        let currentNode = this.body
+        let finished = false;
+        let path = [];
+        let currentNode = this.tree.childNodes[1];
 
         while(!finished){
             for(let i = currentNode.childNodes.length - 1; i >= 0; i--){
                 if(currentNode.childNodes[i].id <= id){
                     currentNode = currentNode.childNodes[i];
-                    finished = currentNode.id === id ? true : false
+                    finished = currentNode.id === id ? true : false;
                     break;
                 }
             }
-            path.push({id: currentNode.id, attributes: currentNode.attributes, tagName:currentNode.tagName})
+            path.push({id: currentNode.id, attributes: currentNode.attributes, tagName:currentNode.tagName});
         }
 
         return path;
